98520ab1c79d3ba5428fedc8533ec847008990fc
Dan Schapira <dsschapira@gmail.com>
2018-02-06 17:48:15 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Learning Express & MongoDB
+
+This project is simply me learning how to work with Express and MongoDB in a simple app.
+
+## Requirements
+
+MongoDB must be installed locally.  Additionally, when executing the MonogoDB daemon, the DB_PORT variable must be set to the
+port that the database is listening on.
---
+++ server/app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const PORT = 3030;
+const DB_PORT = 27017;
+
+const DB_URL = "mongodb://localhost:"+DB_PORT+"/yelp_project";
+var campgrounds = [];
+
+mongoose.connect(DB_URL,function(err){
+    if(err){ 
+       // console.log(err);
+    }
+});
+
+const campSiteSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var CampSite = mongoose.model("CampSite",campSiteSchema);
+//Compile Schema into a model so we can now use create,find, and other methods.
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use( express.static( "public" ));
+
+app.set("view engine", "ejs");
+
+CampSite.find({},function(err,res){
+    if(err){
+        console.log(err);
+    } else{
+        for(var i=0; i<res.length; i++){
+            campgrounds.push(res[i]);
+        }
+    }
+});
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds",function(req,res){
+    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds",function(req,res){
+    const newCampground = {
+        name: req.body.name,
+        image: req.body.image
+    };
+    campgrounds.push(newCampground);
+    //add to campgrounds array so we don't need to re-find from database every time campgrounds page is loaded
+
+    const newDBCampground = new CampSite(newCampground);
+    newDBCampground.save(function(err,res){
+        if(err) console.log(err);
+    });
+
+    res.redirect("campgrounds");
+});
+
+app.get("/campgrounds/new",function(req,res){
+    res.render("new");
+});
+
+app.get("*",function(req,res){
+    res.render("404");
+});
+
+app.listen(PORT, function(){
+    console.log("Server has started.\nListening on Port:",PORT);
+});
